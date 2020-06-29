0216ea1d6e399fc36e45a2e6d47e715d96524c4f
Sanglae Kim <nova0302@hotmail.com>
2020-06-29 13:41:53 +0900

    removed files moved to other folder

--- jsp_basic/holiday_master.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE holiday_master
-       (
-        theday VARCHAR2(10),     
-        thedesc VARCHAR2(10));  
-INSERT INTO holiday_master VALUES
-        ('SMITH',  'CLERK');
-select * from holiday_master;
-desc holiday_master;
-commit;
