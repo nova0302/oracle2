a6d37dca8a017ddcb3028f6c13200c69d26b197d
Sanglae Kim <nova0302@hotmail.com>
2020-11-18 06:29:50 +0900

    made some sql key words to upper case and removed double quotes

--- test_sol_and_instruction_20200629/v_sale.sql
+++ test_sol_and_instruction_20200629/v_sale.sql
@@ -1,10 +1,10 @@
 SET PAGESIZE 50
-COL "saleno" for 99999999
-COL "pcode" for a8
-COL "saledate" for a15
-COL "scode" for a8
-COL "sname" for a8
-COL "name" for a16
+COL saleno FOR 99999999
+COL pcode FOR a8
+COL saledate FOR a15
+COL scode FOR a8
+COL sname FOR a8
+COL name FOR a16
 
 CREATE OR REPLACE VIEW v_sale
 AS
@@ -23,33 +23,33 @@ SELECT * FROM v_sale;
 
 CREATE OR REPLACE VIEW vs0
 AS
-select vs.scode, sum(vs.sub_total) total
-from v_sale vs 
-group by vs.scode;
+SELECT vs.scode, sum(vs.sub_total) total
+FROM v_sale vs 
+GROUP BY vs.scode;
 
 SELECT * FROM vs0;
 
 CREATE OR REPLACE VIEW vs1
 AS
-select t.scode, ts.sname, t.total
-from vs0 t, tbl_shop_01 ts
-where t.scode = ts.scode;
+SELECT t.scode, ts.sname, t.total
+FROM vs0 t, tbl_shop_01 ts
+WHERE t.scode = ts.scode;
 
 SELECT * FROM vs1;
 
 CREATE OR REPLACE VIEW vp0
 AS
-select vs.pcode, sum(sub_total) total
-from v_sale vs, tbl_product_01 tp
-where vs.pcode = tp.pcode
-group by vs.pcode;
+SELECT vs.pcode, sum(sub_total) total
+FROM v_sale vs, tbl_product_01 tp
+WHERE vs.pcode = tp.pcode
+GROUP BY vs.pcode;
 
 SELECT * FROM vp0;
 
 CREATE OR REPLACE VIEW vp1
 AS
-select t.pcode, tp.name, t.total
-from vp0 t, tbl_product_01 tp
-where t.pcode = tp.pcode;
+SELECT t.pcode, tp.name, t.total
+FROM vp0 t, tbl_product_01 tp
+WHERE t.pcode = tp.pcode;
 
 SELECT * FROM vp1;
