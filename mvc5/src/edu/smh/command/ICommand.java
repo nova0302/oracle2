cf21458c0b94bac0f21b9268cb47a1b4ae28d2b7
Sanglae Kim <nova0302@hotmail.com>
2020-11-27 22:57:23 +0900

    mvc5 folder removed

--- mvc5/src/edu/smh/command/ICommand.java
+++
@@ -1,8 +0,0 @@
-package edu.smh.command;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface ICommand {
-	void excute(HttpServletRequest request , HttpServletResponse response);
-}
