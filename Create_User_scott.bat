a3c0010cd7cd2b043c2595148ae80c01ccfec762
Kwanwoo <kwanwoo2450@gmail.com>
2021-06-30 09:48:06 +0900

    Update and rename resetPassword.bat to Create_User_scott.bat

--- Create_User_scott.bat
+++ Create_User_scott.bat
@@ -5,7 +5,10 @@ set local_path=./local.sql
 (
   echo alter user system identified by 1234;
   echo commit;
+  echo CREATE USER scott IDENTIFIED BY tiger;
+  echo GRANT CONNECT, RESOURCE TO scott;
   echo exit;
+  
 ) > %local_path%
 
 sqlplus / as sysdba @%local_path%
